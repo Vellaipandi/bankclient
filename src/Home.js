878362d40d33b0a3985a2c945c7e9bc37bf285ce
vellaipandi <121275841+Vellaipandi@users.noreply.github.com>
2024-03-19 16:12:04 +0530

    bankclient

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+export default function Home(){
+    return(
+        <>
+         <div id="home" className="transparent-box">
+      <h3>Welcome to <span id="span-1">MCA</span>-<span id="span-2">BANK</span></h3>
+      <h4>"Online banking"</h4>
+      <p> Available 24/7 from almost any device with an internet connection</p>
+      <p>Create new Account ,balance checks and deposit your money.</p>
+         </div>
+
+        <div className="fullscreen-image">
+      <img id="firstpage" src="moneybankimg.jpg" alt="First Page" />
+    </div>
+       
+        </>
+    )
+}
